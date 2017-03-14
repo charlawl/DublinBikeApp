93606b80c5a51a5eac3252656811889fff29dbfb
Nkosi Ndlovu <nkosindlovu87@gmail.com>
2017-03-14 16:14:42 +0000

    added map to page, displayed markers, basic styling, turned debug on

--- __init__.py
+++ __init__.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-from misc import x
+# from misc import x
 
 @app.route("/")
 def homepage():
+    json_data = {"name":"Joe","surname":"Doe"}
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True, port=8080, host='localhost')
